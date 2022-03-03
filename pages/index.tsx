921b22c620e8a4242041eeed39ae16b0d332b94e
Dvornick <svc@dvornick.ru>
2022-03-03 10:16:17 +0300

    Add Rating component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Button, Htag, P, Tag } from '../components'
+import { useState } from 'react'
+import { Button, Htag, P, Rating, Tag } from '../components'
 
 const Home: NextPage = () => {
+
+  const [rating, setRating] = useState<number>(4)
+
   return (
     <>
       <Head>
         <title>MyTop</title>
       </Head>
-      <Htag tag='h1'>Text</Htag>
+      <Htag tag='h1'>Заголовок</Htag>
       <Button appearance='primary' arrow='right'>Кнопка</Button>
       <Button appearance='ghost' arrow='down'>Кнопка</Button>
       <P size='s'>Параграф</P>
@@ -19,6 +23,7 @@ const Home: NextPage = () => {
       <Tag className='s' color='green'>Green</Tag>
       <Tag className='s' color='gray'>G ray</Tag>
       <Tag color='primary'>Мал</Tag>
+      <Rating rating={rating} setRating={setRating} isEditable/>
     </>
   )
 }

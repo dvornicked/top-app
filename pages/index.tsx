5b78b2b9a50e2b18f377e6d9860e028310718795
Dvornick <svc@dvornick.ru>
2022-03-07 02:36:07 +0300

    Add Static Props and Static Paths

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import { useState } from 'react'
 import { Button, Htag, P, Rating, Tag } from '../components'
 import { withLayout } from '../layout/Layout'
+import axios from 'axios'
+import { MenuItem } from '../interfaces/menu.interface'
 
-const Home: NextPage = () => {
+const Home: NextPage<HomeProps> = ({menu, firstCategory}): JSX.Element => {
 
   const [rating, setRating] = useState<number>(4)
 
@@ -25,8 +27,27 @@ const Home: NextPage = () => {
       <Tag className='s' color='gray'>G ray</Tag>
       <Tag color='primary'>Мал</Tag>
       <Rating rating={rating} setRating={setRating} isEditable/>
+      <ul>
+        {menu.map(m => <li key={m._id.secondCategory}>{m._id.secondCategory}</li>)}
+      </ul>
     </>
   )
 }
 
 export default withLayout(Home)
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + 'api/top-page/find', {firstCategory: 0})
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  }
+}
+
+interface HomeProps {
+  menu: MenuItem[]
+  firstCategory: number
+}

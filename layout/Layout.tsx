aba9d920e56a629dd16f6dc1cd407397f661f3f9
Dvornick <svc@dvornick.ru>
2022-03-18 23:36:51 +0300

    Add menu accessibility from keyboard

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -5,23 +5,44 @@ import cn from 'classnames'
 import { Header } from './Header/Header'
 import { Sidebar } from './Sidebar/Sidebar'
 import { Footer } from './Footer/Footer'
-import { FC } from 'react'
+import { FC, useRef, useState, KeyboardEvent } from 'react'
 import { AppContextProvider, IAppContext } from '../context/app.context'
 import { Up } from '../components'
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
+  const [isSkipLinkDisplayed, setIsSkipLinkDisplayed] = useState<boolean>(false)
+  const bodyRef = useRef<HTMLDivElement>(null)
+  const skipContentAction = (key: KeyboardEvent) => {
+    if (key.code == 'Space' || key.code == 'Enter') {
+      key.preventDefault()
+      bodyRef.current?.focus()
+    }
+    setIsSkipLinkDisplayed(false)
+  }
   return (
     <div className={styles.wrapper}>
+      <a
+        onFocus={() => setIsSkipLinkDisplayed(true)}
+        onKeyDown={skipContentAction}
+        tabIndex={1}
+        className={cn(styles.skipLink, {
+          [styles.displayed]: isSkipLinkDisplayed
+        })}
+      >
+        Сразу к содержанию
+      </a>
       <Header className={styles.header} />
       <Sidebar className={styles.sidebar} />
-      <div className={styles.body}>{children}</div>
+      <div ref={bodyRef} tabIndex={0} className={styles.body}>{children}</div>
       <Footer className={styles.footer} />
       <Up />
     </div>
   )
 }
 
-export const withLayout = <T extends Object & IAppContext>(Component: FC<T>) => {
+export const withLayout = <T extends Object & IAppContext>(
+  Component: FC<T>
+) => {
   return function withLayoutComponent(props: T): JSX.Element {
     return (
       <AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
